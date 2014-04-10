5a52ebdb530aec4c89796af1371918f834227b80
Denis Kaynarca <denis@dkaynarca.com>
2014-04-10 18:25:47 +1000

    Added the IHardware interface inside a common namespace/project.

---
+++ src/cluster/Tmc/Common/IHardware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tmc.Common
+{
+    enum HardwareStatus
+    {
+        Offline,
+        Operational,
+        Failed
+    }
+
+    public interface IHardware
+    {
+        string Name { get; set; }
+        HardwareStatus GetHardwareStatus();
+    }
+}
