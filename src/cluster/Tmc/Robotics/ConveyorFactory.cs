249193c8c2d69165524f3cbf7e3e1196e94b6cda
Henry Quach <henry_quach91@hotmail.com>
2014-04-14 12:24:14 +1000

    Added initial conveyor code

---
+++ src/cluster/Tmc/Robotics/ConveyorFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tmc.Robotics
+{
+    public static class ConveyorFactory
+    {
+        public static T CreateFactory<T>() where T : class, IRobot
+        {
+            var caseSwitch = new Dictionary<Type, Func<T>>
+            {
+                {typeof(BluetoothConveyor)
+            }
+            return caseSwitch[typeof(T)]();
+        }
+
+        private void BluetoothConveyor BuildBluetoothConveyor()
+        {
+            return new BluetoothConveyor();
+        }
+    }
+}
---
+++ src/cluster/Tmc/Robotics/Conveyors/BluetoothConveyor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tmc.Common;
+
+namespace Tmc.Robotics
+{
+    public class BluetoothConveyor : IConveyor
+    {
+        public string Name { get; set; }
+
+        public HardwareStatus GetStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetParameters(Dictionary<string, string> parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
