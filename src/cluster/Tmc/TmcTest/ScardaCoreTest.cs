df3eda82ca173c3922de4755861396ec6f615cef
Denis R <ruan_Denis76@hotmail.com>
2014-04-15 00:30:13 +1000

    Added Testing Solution and Sample Test

---
+++ src/cluster/Tmc/TmcTest/ScardaCoreTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TmcTest
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
