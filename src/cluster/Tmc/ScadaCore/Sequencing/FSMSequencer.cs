41bf8e1b16e5c02fba7c6e471f5d1fa5cd089a2e
Denis Kaynarca <denis@dkaynarca.com>
2014-04-17 17:04:55 +1000

    Added main "happy path" state machine logic.

--- src/cluster/Tmc/ScadaCore/Sequencing/FSMSequencer.cs
+++ src/cluster/Tmc/ScadaCore/Sequencing/FSMSequencer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Linq;
+using System.Diagnostics;
 using Appccelerate.StateMachine;
 using Appccelerate.StateMachine.Extensions;
 
@@ -13,9 +14,32 @@ namespace Tmc.Scada.Core.Sequencing
         public bool Enabled { get; private set; }
 
         private PassiveStateMachine<State, Trigger> _fsm;
+        private ScadaEngine _engine;
+        private ConveyorController _conveyorController;
+        private Assembler _assembler;
+        private Loader _loader;
+        private Sorter _sorter;
+        private TrayVerifier _trayVerifier;
+        private Palletiser _palletiser;
 
-        public FSMSequencer()
+        public FSMSequencer(ScadaEngine engine)
         {
+            this._engine = engine;
+
+            Debug.Assert(this._engine != null);
+
+            this._conveyorController = _engine.ClusterConfig.Controllers[typeof(ConveyorController)] as ConveyorController;
+            this._assembler = _engine.ClusterConfig.Controllers[typeof(Assembler)] as Assembler;
+            this._loader = _engine.ClusterConfig.Controllers[typeof(Loader)] as Loader;
+            this._trayVerifier = _engine.ClusterConfig.Controllers[typeof(TrayVerifier)] as TrayVerifier;
+            this._palletiser = _engine.ClusterConfig.Controllers[typeof(Palletiser)] as Palletiser;
+
+            Debug.Assert(this._conveyorController != null);
+            Debug.Assert(this._assembler != null);
+            Debug.Assert(this._loader != null);
+            Debug.Assert(this._trayVerifier != null);
+            Debug.Assert(this._palletiser != null);
+
             _fsm = new PassiveStateMachine<State, Trigger>();
             Create();
         }
@@ -68,18 +92,7 @@ namespace Tmc.Scada.Core.Sequencing
                 .WithSubState(State.SortingConveyorMovingForward)
                 .WithSubState(State.SortingConveyorMovingBackward);
         }
-
-        public void Resume()
-        {
-            _fsm.Fire(Trigger.Resume);
-        }
-
-        private void TestStop()
-        {
-            _fsm.Fire(Trigger.Stop);
-            _fsm.Fire(Trigger.Resume);
-        }
-
+        
         private void CreateSortingStates()
         {
             _fsm.In(State.Sorting)
@@ -151,10 +164,10 @@ namespace Tmc.Scada.Core.Sequencing
 
             _fsm.In(State.AssemblyConveyorMovingForward)
                 .On(Trigger.Completed)
-                    .If(() => true)
-                        .Goto(State.Assembling)
-                    .If(() => false)
+                    .If(() => _conveyorController.CanMoveForward(ConveyorType.Assembly))
                         .Goto(State.VerifyingTray)
+                    .Otherwise()
+                        .Goto(State.Assembling)
                 .On(Trigger.Stop)
                     .Goto(State.Stopped)
                 .On(Trigger.Shutdown)
@@ -180,9 +193,9 @@ namespace Tmc.Scada.Core.Sequencing
 
             _fsm.In(State.AssemblyConveyorMovingBackward)
                 .On(Trigger.Completed)
-                    .If(() => true)
+                    .If(() => _conveyorController.CanMoveBackward(ConveyorType.Assembly))
                         .Goto(State.VerifyingTray)
-                    .If(() => false)
+                    .Otherwise()
                         .Goto(State.PlacingTrayInBuffer)
                 .On(Trigger.Stop)
                     .Goto(State.Stopped)
