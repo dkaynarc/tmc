fc51d1d3b8895d1a542375813e0ff9dd009fea8b
Denis Kaynarca <denis@dkaynarca.com>
2014-04-20 16:32:32 +1000

    Implemented Loader and Palletiser controllers. The Loader still needs the PLC so it can select which tray to load. It also currently assumes that there is only one way to palletise.

--- src/cluster/Tmc/ScadaCore/Controllers/Loader.cs
+++ src/cluster/Tmc/ScadaCore/Controllers/Loader.cs
@@ -3,25 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tmc.Robotics;
 
 namespace Tmc.Scada.Core
 {
-    public class Loader : ControllerBase
-    {
-        public Loader(ClusterConfig config) : base(config)
-        {
-        }
-
-        public override void Begin(ControllerParams parameters)
-        {
-            var p = parameters as LoaderParams;
-            if (p != null)
-            {
-
-            }
-        }
-    }
-
     public enum LoaderAction
     {
         LoadToConveyor,
@@ -29,6 +14,80 @@ namespace Tmc.Scada.Core
         LoadToRejectedBuffer
     }
 
+    public class Loader : ControllerBase
+    {
+        private LoaderRobot _loaderRobot;
+        //private PlcSensor _traySensor;
+        private Dictionary<LoaderAction, Action> _actionMap;
+
+        public Loader(ClusterConfig config) : base(config)
+        {
+            _actionMap = new Dictionary<LoaderAction, Action>
+            {
+                { LoaderAction.LoadToConveyor, () => LoadToConveyorAsync() },
+                { LoaderAction.LoadToAcceptedBuffer, () => LoadToAccpetedBufferAsync() },
+                { LoaderAction.LoadToRejectedBuffer, () => LoadToRejectedBufferAsync() }
+            };
+
+            _loaderRobot = config.Robots[typeof(LoaderRobot)] as LoaderRobot;
+
+            if (_loaderRobot == null)
+            {
+                throw new ArgumentException("Could not retrieve a LoaderRobot from ClusterConfig");
+            }
+        }
+
+        public override void Begin(ControllerParams parameters)
+        {
+            var p = parameters as LoaderParams;
+            if (p != null)
+            {
+                if (!IsRunning)
+                {
+                    IsRunning = true;
+                    _actionMap[p.Action]();
+                }
+            }
+        }
+
+        private int SelectShelf()
+        {
+            // TODO: write an algorithm to select the first available tray in the magazine
+            // Requires the PLC
+            return 0;
+        }
+
+        private void LoadToConveyorAsync()
+        {
+            var task = new Task(() =>
+                {
+                    var shelf = SelectShelf();
+                    _loaderRobot.GetTray(shelf);
+                    IsRunning = false;
+                });
+            task.Start();
+        }
+
+        private void LoadToAccpetedBufferAsync()
+        {
+            var task = new Task(() =>
+                {
+                    _loaderRobot.Palletise();
+                    IsRunning = false;
+                });
+            task.Start();
+        }
+
+        private void LoadToRejectedBufferAsync()
+        {
+            var task = new Task(() =>
+                {
+                    _loaderRobot.Palletise();
+                    IsRunning = false;
+                });
+            task.Start();
+        }
+    }
     public class LoaderParams : ControllerParams
     {
         public LoaderAction Action { get; set; }
--- src/cluster/Tmc/ScadaCore/Controllers/Palletiser.cs
+++ src/cluster/Tmc/ScadaCore/Controllers/Palletiser.cs
@@ -3,17 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tmc.Robotics;
 
 namespace Tmc.Scada.Core
 {
     public class Palletiser : ControllerBase
     {
+        private PalletiserRobot _palletiserRobot;
         public Palletiser(ClusterConfig config) : base(config)
         {
+            _palletiserRobot = config.Robots[typeof(PalletiserRobot)] as PalletiserRobot;
+
+            if (_palletiserRobot == null)
+            {
+                throw new ArgumentException("Could not retrieve a PalletiserRobot from ClusterConfig");
+            }
         }
 
         public override void Begin(ControllerParams parameters)
         {
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                PalletiseAsync();
+            }
+        }
+
+        private void PalletiseAsync()
+        {
+            var task = new Task(() =>
+                {
+                    _palletiserRobot.Palletise();
+                    IsRunning = false;
+                });
+            task.Start();
         }
     }
 }
