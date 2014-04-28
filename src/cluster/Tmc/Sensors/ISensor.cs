08be6b378117dc4d3349a2862af66ffc5802d64a
donaru <chan_donald@hotmail.com>
2014-04-28 12:22:09 +1000

    Update class file structure

--- src/cluster/Tmc/Sensors/ISensor.cs
+++ src/cluster/Tmc/Sensors/ISensor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace Tmc.Sensors
 {
     public interface ISensor : IHardware
     {
+        string ConnectionString { get; set; }
     }
 }
