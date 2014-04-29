03a4874883d00404a6b4be4837b6292fc1a86c91
Denis Kaynarca <denis@dkaynarca.com>
2014-04-29 18:40:40 +1000

    Added db connection string

--- src/cluster/Tmc/ScadaApp/Program.cs
+++ src/cluster/Tmc/ScadaApp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tmc.Scada.Core;
+using TmcData;
 
 namespace Tmc.Scada.App
 {
@@ -16,6 +17,7 @@ namespace Tmc.Scada.App
         static void Main()
         {
             //IScada s = new ScadaEngine();
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
