a3890c445a582410972443f08269dc0984ceffa2
Denis Kaynarca <denis@dkaynarca.com>
2014-05-02 00:22:18 +1000

    Added Tray class

--- src/cluster/Tmc/Common/Tray.cs
+++ src/cluster/Tmc/Common/Tray.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tmc.Scada.Core
+namespace Tmc.Common
 {
-    public class Tray
+    public class Tray<T>
     {
+        public const int MaxCells = 9;
+
+        public List<T> Cells { get; set; }
+
+        public Tray()
+        {
+            Cells = new List<T>(MaxCells);
+        }
     }
 }
