36f5bf01c4ef065d00584a691691a80e8f22cdb0
sergik80 <tyrkamotchka@gmail.com>
2014-05-01 23:31:05 -0300

    added support for json message parsing; completed authentication with username persistence

--- src/mobile/TMC/src/services/SynchService.java
+++ src/mobile/TMC/src/services/SynchService.java
@@ -2,6 +2,8 @@ package services;
 
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,7 +15,7 @@ import android.os.Bundle;
 
 public class SynchService extends IntentService 
 {
-    private String urlString = "http://192.168.1.3:9000/api/Server/";
+    private String urlString = "http://192.168.1.5:9000/api/Server/";
 	// private String urlString = "http://172.19.14.183:9000/api/Server/";    
      private int command;
 
@@ -103,27 +105,27 @@ private void authenticate(Bundle parcel)
 	{
     	StringBuilder str = new StringBuilder();
      
-    	try{
-		  
+    	
+	 try{ 
 		  URL url = new URL(urlStr);
 		  HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		  con.setRequestMethod("GET");
-		  con.setConnectTimeout(10000); // times out after 10 seconds
+		  con.setConnectTimeout(100000); // times out after 10 seconds
 		  //con.setRequestProperty("Content-Type","application/json");
 		  con.setRequestProperty("Accept", "application/json");
-	      BufferedInputStream in = new BufferedInputStream(con.getInputStream());
+	      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
    
-	   
-	      // read result
-		  byte[] contents = new byte[1024];
-		  int flag;
+
+		  String line;
 		 
-	      while((flag = in.read(contents)) !=-1 )
+	      while((line = in.readLine()) !=null )
 		  {
-		    str.append(new String(contents,0,flag));
+		    str.append(line);
 		  }	
-		
+		  in.close();
 	    }
+	 
+	 
 	 catch(Exception exc)
 	 {
 	   System.out.print("Exception: " + exc.toString());
