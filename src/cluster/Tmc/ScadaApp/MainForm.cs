441943856de6722dd372650760563914e1d97466
Denis Kaynarca <denis@dkaynarca.com>
2014-05-04 16:24:21 +1000

    Added initial Plc sensor class. This is super experimental at the moment and needs rigorous testing against the actual hardware. I've left initialisation code in ScadaCore.MainForm. This will need to be removed during integration.

--- src/cluster/Tmc/ScadaApp/MainForm.cs
+++ src/cluster/Tmc/ScadaApp/MainForm.cs
@@ -7,16 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tmc.Sensors;
 
 
 namespace Tmc.Scada.App
 {
     public partial class MainForm : Form
     {
+        private Plc _plc;
         public MainForm()
         {
             InitializeComponent();
 
+            _plc = new Plc();
+            _plc.SetParameters(new Dictionary<string, string>
+            {
+                { "Name", "PLC" },
+                { "IPAddress", "192.168.1.5"}
+            });
+            try
+            {
+                _plc.Initialise();
+                var switchStates = _plc.GetSwitchStates();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
     }
 }
---
+++ src/cluster/Tmc/Sensors/Plc.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASADTCPLib;
+using Tmc.Common;
+using AxASADTCPLib;
+
+namespace Tmc.Sensors
+{
+    public enum PlcAttachedSwitch
+    {
+        Tray4 = 0,
+        Tray1,
+        Tray2,
+        Tray3,
+        Tray5,
+        Tray6,
+        PlcEmergencyStop,
+        ScadaEmergencyStop
+    }
+
+    public class Plc : IHardware
+    {
+        private HardwareStatus _hwStatus;
+        private string _nodeIpAddress;
+        private AxAsadtcp _plc;
+        private Dictionary<PlcAttachedSwitch, bool> _switchStates;
+
+        public Plc()
+        {
+            _switchStates = new Dictionary<PlcAttachedSwitch,bool>();
+            foreach (var item in (PlcAttachedSwitch[])Enum.GetValues(typeof(PlcAttachedSwitch)))
+            {
+                _switchStates.Add(item, false);
+            }
+        }
+
+        public string Name { get; set; }
+
+        public HardwareStatus GetStatus()
+        {
+            return this._hwStatus;
+        }
+
+        public void Initialise()
+        {
+            try
+            {
+                this._plc = CreatePlcControl();
+                ConnectToPlc();
+            }
+            catch (Exception e)
+            {
+                this._hwStatus = HardwareStatus.Failed;
+                throw e;
+            }
+            this._hwStatus = HardwareStatus.Operational;
+        }
+
+        public void Shutdown()
+        {
+            this._plc.Disconnect();
+            this._hwStatus = HardwareStatus.Offline;
+        }
+
+        public void SetParameters(Dictionary<string, string> parameters)
+        {
+            string param;
+            if (!parameters.TryGetValue("IPAddress", out param))
+            {
+                throw new Exception("PLC requires an IPAddress parameter");
+            }
+
+            this._nodeIpAddress = param;
+
+            if (parameters.TryGetValue("Name", out param))
+            {
+                this.Name = param;
+            }
+        }
+
+        public Dictionary<PlcAttachedSwitch, bool> GetSwitchStates()
+        {
+            UpdateAllSwitchStates();
+            return this._switchStates;
+        }
+
+        private void UpdateAllSwitchStates()
+        {
+            this._plc.SyncRefresh();
+            var switchKeysArray = _switchStates.Keys.ToArray();
+            for (int i = 0; i < switchKeysArray.Count(); i++)
+            {
+                var switchKey = switchKeysArray[i];
+                _switchStates[switchKey] = this._plc.GetDataBitM((short)switchKey);
+            }
+        }
+
+        private AxAsadtcp CreatePlcControl()
+        {
+            var control = new AxAsadtcp();
+
+            ((System.ComponentModel.ISupportInitialize)(control)).BeginInit();
+            control.CreateControl();
+            control.Visible = false;
+            control.NodeAddress = _nodeIpAddress;
+            ((System.ComponentModel.ISupportInitialize)(control)).EndInit();
+            
+            return control;
+        }
+
+        private void ConnectToPlc()
+        {
+            this._plc.MemStart = "Y0";
+            this._plc.MemQty = 1;
+        }
+    }
+}
