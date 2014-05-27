4d68ae288970f6bea5120876eb12d8542aa2d68e
sergik80 <tyrkamotchka@gmail.com>
2014-05-27 09:29:22 -0300

    added APIServerWeb project

---
+++ src/cluster/Tmc/APIServerWeb/APIServerWeb/Authentication/SCADAUserManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ApiServerWeb.Authentication
+{
+    public class SCADAUserManager : UserManager<SCADAUser>
+    {
+        private int minimumPasswordLength = 1;
+
+
+        public SCADAUserManager()
+            : base(new UserStore<SCADAUser>(new SCADADbContext()))
+        {
+            PasswordValidator = new MinimumLengthValidator(minimumPasswordLength);
+        }
+    }
+}
---
+++ src/cluster/Tmc/APIServerWeb/APIServerWeb/Parcels/DeleteOrderParcel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIServerWeb.Parcels
+{
+    public class DeleteOrderParcel
+    {
+        public string orderId { get; set; }
+        public string result { get; set; }
+    }
+}
