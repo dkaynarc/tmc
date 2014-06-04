97313ac11dffd17a6a51e222e67cfe3739903435
Shale Kuzmanovski <Shale.lol@hotmail.com>
2014-06-04 20:40:53 +1000

    *  Changed GetLatestAlarm to return the most recent Warnings and errors.

--- src/cluster/Tmc/TmcData/TmcRepository.cs
+++ src/cluster/Tmc/TmcData/TmcRepository.cs
@@ -68,8 +68,7 @@ namespace TmcData
 
         public static ComponentEventLogView GetLatestAlarm()
         {
-            throw new NotImplementedException();
-            //_entities.ComponentEventLogViews.Where(e => e.LogType = LogType);
+            return _entities.ComponentEventLogViews.Where(e => e.LogType == LogType.Warning.ToString() || e.LogType == LogType.Error.ToString()).OrderByDescending(e => e.Timestamp).First();
         }
 
         public static void CancelOrder(int orderID)
