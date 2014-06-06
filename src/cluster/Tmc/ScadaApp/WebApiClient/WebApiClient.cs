48c8fcfcfb0e4a7e640321894c409323f94bb6b2
Shale Kuzmanovski <Shale.lol@hotmail.com>
2014-06-06 13:46:34 +1000

    + Added WebApiClient to scada app for user authentication. + Added restsharp package to scadaapp

---
+++ src/cluster/Tmc/ScadaApp/WebApiClient/WebApiClient.cs
@@ -0,0 +1,39 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tmc.Scada.App
+{
+    public class WebApiClient
+    {
+        private RestClient _client;
+
+        public WebApiClient(string url)
+        {
+            _client = new RestClient(url);
+        }
+
+        public bool AddUser(string username, string password, string roleName)
+        {
+            var request = new RestRequest("api/server/adduser/{userName}/{password}/{rolename}");
+            request.AddUrlSegment("userName", username);
+            request.AddUrlSegment("password", password);
+            request.AddUrlSegment("rolename", roleName);
+
+            return _client.Execute(request).Content == "success";   
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            var request = new RestRequest("api/server/authenticate/{username}/{password}");
+            request.AddUrlSegment("username", username);
+            request.AddUrlSegment("password", password);
+
+            return _client.Execute(request).Content == "success";
+        }
+    }
+}
