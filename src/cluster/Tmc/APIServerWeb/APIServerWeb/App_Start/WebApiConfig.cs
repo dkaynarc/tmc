58a7be1cb7071d0fbd767bf0f0a257c8057d61f0
sergik80 <tyrkamotchka@gmail.com>
2014-06-07 00:17:26 -0300

    added latest APIServr files

---
+++ src/cluster/Tmc/APIServerWeb/APIServerWeb/App_Start/WebApiConfig.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+
+
+
+
+
+
+
+
+
+    public static class WebApiConfig
+    {
+
+
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(name: "route3",
+              routeTemplate: "api/{controller}/{action}/{var1}/{var2}",
+              defaults: null);
+
+
+            //used to delete an order; used to delete a user
+            config.Routes.MapHttpRoute(name: "route4",
+            routeTemplate: "api/{controller}/{action}/{var}",
+            defaults: null);
+
+
+            config.Routes.MapHttpRoute(name: "route5",
+            routeTemplate: "api/{controller}/{action}/{var1}/{var2}/{var3}/{var4}/{var5}/{var6}",
+            defaults: null);
+
+
+
+            config.Routes.MapHttpRoute(
+                       name: "ModifyOrderRoute",
+                       routeTemplate:
+                       "api/{controller}/{action}/{orderName}/{orderId}/{status}/{black}/{blue}/{green}/{red}/{white}",
+                       defaults: null);
+
+
+            config.Routes.MapHttpRoute(
+                     name: "Machine",
+                     routeTemplate: "api/{controller}/{action}",
+                     defaults: null);
+
+
+
+            config.Routes.MapHttpRoute(
+                                 name: "Default",
+                                 routeTemplate: "api/{controller}/{id}",
+                                 defaults: new { id = RouteParameter.Optional });
+
+
+            config.Routes.MapHttpRoute(
+                                            null,
+                                            "",
+                                            new { controller = "Server" });
+
+
+            config.MapHttpAttributeRoutes();
+
+
+
+            config.Routes.MapHttpRoute(name: "newUserRoute",
+            routeTemplate: "api/{controller}/{action}/{userName}/{password}/{roleName}",
+            defaults: null); 
+
+
+
+        }
+    }
+
---
+++ src/cluster/Tmc/APIServerWeb/APIServerWeb/Parcels/OrderParcel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIServerWeb.Parcels
+{
+
+    public class OrderParcel : Parcel
+    {
+        public int mOrderId { get; set; }
+        public string mOrderOwner { get; set; }
+        public string mOrderStatus { get; set; }
+        public int black { get; set; }
+        public int blue { get; set; }
+        public int green { get; set; }
+        public int red { get; set; }
+        public int white { get; set; }
+
+        public string startTime { get; set; }
+
+        public string endTime { get; set; }
+    }
+}
+
