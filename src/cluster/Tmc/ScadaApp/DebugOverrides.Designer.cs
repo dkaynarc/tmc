2721c686c16f7a9d37e11cea28e0fc102addcbda
Denis Kaynarca <denis@dkaynarca.com>
2014-06-08 19:01:47 +1000

    * Added *draft* debug overrides page

---
+++ src/cluster/Tmc/ScadaApp/DebugOverrides.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Tmc.Scada.App
+{
+    partial class DebugOverrides
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbTabMag = new System.Windows.Forms.GroupBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this._tabMagBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbBlack = new System.Windows.Forms.TextBox();
+            this.tbBlue = new System.Windows.Forms.TextBox();
+            this.tbGreen = new System.Windows.Forms.TextBox();
+            this.tbRed = new System.Windows.Forms.TextBox();
+            this.tbWhite = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gbTabMag.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._tabMagBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbTabMag
+            // 
+            this.gbTabMag.Controls.Add(this.label5);
+            this.gbTabMag.Controls.Add(this.label4);
+            this.gbTabMag.Controls.Add(this.label3);
+            this.gbTabMag.Controls.Add(this.label2);
+            this.gbTabMag.Controls.Add(this.label1);
+            this.gbTabMag.Controls.Add(this.tbWhite);
+            this.gbTabMag.Controls.Add(this.tbRed);
+            this.gbTabMag.Controls.Add(this.tbGreen);
+            this.gbTabMag.Controls.Add(this.tbBlue);
+            this.gbTabMag.Controls.Add(this.tbBlack);
+            this.gbTabMag.Controls.Add(this.btnUpdate);
+            this.gbTabMag.Location = new System.Drawing.Point(3, 3);
+            this.gbTabMag.Name = "gbTabMag";
+            this.gbTabMag.Size = new System.Drawing.Size(177, 214);
+            this.gbTabMag.TabIndex = 0;
+            this.gbTabMag.TabStop = false;
+            this.gbTabMag.Text = "Tablet Magazine";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(6, 163);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // tbBlack
+            // 
+            this.tbBlack.Location = new System.Drawing.Point(71, 19);
+            this.tbBlack.Name = "tbBlack";
+            this.tbBlack.Size = new System.Drawing.Size(100, 20);
+            this.tbBlack.TabIndex = 11;
+            // 
+            // tbBlue
+            // 
+            this.tbBlue.Location = new System.Drawing.Point(71, 46);
+            this.tbBlue.Name = "tbBlue";
+            this.tbBlue.Size = new System.Drawing.Size(100, 20);
+            this.tbBlue.TabIndex = 12;
+            // 
+            // tbGreen
+            // 
+            this.tbGreen.Location = new System.Drawing.Point(71, 73);
+            this.tbGreen.Name = "tbGreen";
+            this.tbGreen.Size = new System.Drawing.Size(100, 20);
+            this.tbGreen.TabIndex = 13;
+            // 
+            // tbRed
+            // 
+            this.tbRed.Location = new System.Drawing.Point(71, 100);
+            this.tbRed.Name = "tbRed";
+            this.tbRed.Size = new System.Drawing.Size(100, 20);
+            this.tbRed.TabIndex = 14;
+            // 
+            // tbWhite
+            // 
+            this.tbWhite.Location = new System.Drawing.Point(71, 127);
+            this.tbWhite.Name = "tbWhite";
+            this.tbWhite.Size = new System.Drawing.Size(100, 20);
+            this.tbWhite.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Black";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Blue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Green";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Red";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "White";
+            // 
+            // DebugOverrides
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbTabMag);
+            this.Name = "DebugOverrides";
+            this.Size = new System.Drawing.Size(186, 222);
+            this.gbTabMag.ResumeLayout(false);
+            this.gbTabMag.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._tabMagBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbTabMag;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.BindingSource _tabMagBindingSource;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbWhite;
+        private System.Windows.Forms.TextBox tbRed;
+        private System.Windows.Forms.TextBox tbGreen;
+        private System.Windows.Forms.TextBox tbBlue;
+        private System.Windows.Forms.TextBox tbBlack;
+
+    }
+}
