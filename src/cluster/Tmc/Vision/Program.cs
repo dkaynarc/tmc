c049dead095bdd3ec4eca85acbb4c7bf1e8e6c10
Leonid Soukhankine <leonid_246@hotmail.com>
2014-06-09 00:48:17 +1000

    tray detection detect the yellow line by doing hough transform now much more acurate

--- src/cluster/Tmc/Vision/Program.cs
+++ src/cluster/Tmc/Vision/Program.cs
@@ -29,20 +29,24 @@ namespace Tmc.Vision
         private static void TestCamera()
         {
             Camera trayC = new Camera();
-            trayC.ConnectionString = new Uri(@"http://192.168.1.195:8080/photoaf.jpg");
+            trayC.ConnectionString = new Uri(@"http://192.168.1.8:8080/photoaf.jpg");
             Camera sort = new Camera();
             sort.ConnectionString = new Uri(@"http://192.168.1.195:8080/photoaf.jpg");
             //c.SetParameters(new Dictionary<string, string> { { "Name", "TestCam1" }, { "ConnectionString", "http://192.168.0.11:8080/photo.jpg" } });//"http://192.168.0.11:8080/photo.jpg"
-            //trayC.Initialise();
+            trayC.Initialise();
             //sort.Initialise();
-            //TrayDetectorVision tray = new TrayDetectorVision(trayC);
-            //Tray<Tablet> Tray = tray.GetTabletsInTray();
-            SorterVision sorter = new SorterVision(sort);
+            TrayDetectorVision tray = new TrayDetectorVision(trayC);
+            while (true)
+            {
+                Tray<Tablet> Tray = new Tray<Tablet>();
+                 bool colr = tray.GetTabletsInTray(out Tray);
+            }
+            /*SorterVision sorter = new SorterVision(sort);
             sorter.Calibrate();
             while (true)
             {
                 List<Tablet> tabletList = sorter.GetVisibleTablets();
-            }
+            }*/
             //CvInvoke.cvWaitKey(0);
         }
     }
